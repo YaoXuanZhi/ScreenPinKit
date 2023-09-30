6bdb7c6fcb9c11607e00acdbe9f55eec0b83e022
yaoxuanzhi <yaoxuanzhi@outlook.com>
2023-09-30 14:37:52 +0800

    设置scene background

--- src/main.py
+++ src/main.py
@@ -23,6 +23,7 @@ class MainWindow(QWidget):
         scene = CanvasScene()
         rectItem = scene.addRect(0, 0, 100, 100)
         rectItem.setFlags(QGraphicsItem.ItemIsMovable | QGraphicsItem.ItemIsSelectable)
+        rectItem.setPen(QPen(Qt.red))
         view = CanvasView(scene)
         self.layout.addWidget(view)
 
