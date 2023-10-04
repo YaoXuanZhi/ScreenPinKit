ef9062cbc13dbd84222d7f9529842bea28712870
yaoxuanzhi <yaoxuanzhi@outlook.com>
2023-10-05 02:10:14 +0800

    优化控制点的位置点实现

--- src/canvas_item/canvas_editable_rect.py
+++ src/canvas_item/canvas_editable_rect.py
@@ -1,10 +1,21 @@
 import typing
+from enum import Enum
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from PyQt5.QtWidgets import QGraphicsSceneDragDropEvent, QGraphicsSceneHoverEvent, QStyleOptionGraphicsItem, QWidget
+from PyQt5.QtWidgets import QGraphicsSceneDragDropEvent, QGraphicsSceneHoverEvent, QGraphicsSceneMouseEvent, QStyleOptionGraphicsItem, QWidget
 import sys
+
+class EnumPosType(Enum):
+    ControllerPosTL = 1
+    ControllerPosTC = 2
+    ControllerPosTR = 3
+    ControllerPosRC = 4
+    ControllerPosBR = 5
+    ControllerPosBC = 6
+    ControllerPosBL = 7
+    ControllerPosLC = 8
 
 class CanvasEllipseItem(QGraphicsEllipseItem):
     def __init__(self, interfaceCursor:QCursor, parent:QGraphicsItem = None) -> None:
@@ -37,16 +48,68 @@ class CanvasEllipseItem(QGraphicsEllipseItem):
             option.state = QStyle.StateFlag.State_None
         return super().paint(painter, option, widget)
 
-class CanvasEditableRect(QGraphicsRectItem):
-    ControllerPosTL = 1
-    ControllerPosTC = 2
-    ControllerPosTR = 3
-    ControllerPosRC = 4
-    ControllerPosBR = 5
-    ControllerPosBC = 6
-    ControllerPosBL = 7
-    ControllerPosLC = 8
+    def getControllerPosition(self, rect:QRectF, radius, posType:EnumPosType):
+        if posType == EnumPosType.ControllerPosTL:
+            # 左上角
+            return rect.topLeft() - QPointF(radius, radius)
+        elif posType == EnumPosType.ControllerPosTC:
+            # 顶端居中
+            return (rect.topLeft() + rect.topRight()) / 2 - QPointF(radius, radius)
+        elif posType == EnumPosType.ControllerPosTR:
+            # 右上角
+            return rect.topRight() - QPointF(radius, radius)
+        elif posType == EnumPosType.ControllerPosRC:
+            # 右侧居中
+            return (rect.topRight() + rect.bottomRight()) / 2 - QPointF(radius, radius)
+        elif posType == EnumPosType.ControllerPosBL:
+            # 左下角
+            return rect.bottomLeft() - QPointF(radius, radius)
+        elif posType == EnumPosType.ControllerPosBC:
+            # 底端居中
+            return (rect.bottomLeft() + rect.bottomRight()) / 2 - QPointF(radius, radius)
+        elif posType == EnumPosType.ControllerPosBR:
+            # 右下角
+            return rect.bottomRight() - QPointF(radius, radius)
+        elif posType == EnumPosType.ControllerPosLC:
+            # 左侧居中
+            return (rect.topLeft() + rect.bottomLeft()) / 2 - QPointF(radius, radius)
 
+    def setRectWrapper(self, attachRect:QRectF, posType:EnumPosType, radius:float, size:QSizeF):
+        self.posType = posType
+        pos = self.getControllerPosition(attachRect, radius, posType)
+        self.setRect(QRectF(pos, size))
+
+    def resetPosition(self, attachRect:QRectF, radius:float, size:QSizeF):
+        pos = self.getControllerPosition(attachRect, radius, self.posType)
+        self.setRect(QRectF(pos, size))
+
+    def mouseMoveEvent(self, event: QGraphicsSceneMouseEvent) -> None:
+        parentItem:CanvasEditableRect = self.parentItem()
+
+        newRect = parentItem.boundingRect()
+        if self.posType == EnumPosType.ControllerPosTL:
+            newRect.setTopLeft(event.pos())
+        elif self.posType == EnumPosType.ControllerPosTC:
+            newRect.setTop(event.pos().y())
+        elif self.posType == EnumPosType.ControllerPosTR:
+            newRect.setTopRight(event.pos())
+        elif self.posType == EnumPosType.ControllerPosRC:
+            newRect.setRight(event.pos().x())
+        elif self.posType == EnumPosType.ControllerPosBR:
+            newRect.setBottomRight(event.pos())
+        elif self.posType == EnumPosType.ControllerPosBC:
+            newRect.setBottom(event.pos().y())
+        elif self.posType == EnumPosType.ControllerPosBL:
+            newRect.setBottomLeft(event.pos())
+        elif self.posType == EnumPosType.ControllerPosLC:
+            newRect.setLeft(event.pos().x())
+
+        parentItem.setRect(newRect)
+        # print(f" {__class__.__name__}:{sys._getframe().f_code.co_name} ====> {newRect} / {event.pos()} ")
+        print(f"====> {newRect} / {event.pos()} / {self.pos()} ")
+        parentItem.initControllers()
+
+class CanvasEditableRect(QGraphicsRectItem):
     def __init__(self, rect: QRectF, parent:QGraphicsItem = None) -> None:
         super().__init__(rect, parent)
 
@@ -62,53 +125,31 @@ class CanvasEditableRect(QGraphicsRectItem):
 
         self.initUI()
 
-    def getControllerPosition(self, rect:QRectF, radius, posType:int):
-        if posType == self.ControllerPosTL:
-            # 左上角
-            return rect.topLeft() - QPointF(radius, radius)
-        elif posType == self.ControllerPosTC:
-            # 顶端居中
-            return (rect.topLeft() + rect.topRight()) / 2 - QPointF(radius, radius)
-        elif posType == self.ControllerPosTR:
-            # 右上角
-            return rect.topRight() - QPointF(radius, radius)
-        elif posType == self.ControllerPosRC:
-            # 右侧居中
-            return (rect.topRight() + rect.bottomRight()) / 2 - QPointF(radius, radius)
-        elif posType == self.ControllerPosBL:
-            # 左下角
-            return rect.bottomLeft() - QPointF(radius, radius)
-        elif posType == self.ControllerPosBC:
-            # 底端居中
-            return (rect.bottomLeft() + rect.bottomRight()) / 2 - QPointF(radius, radius)
-        elif posType == self.ControllerPosBR:
-            # 右下角
-            return rect.bottomRight() - QPointF(radius, radius)
-        elif posType == self.ControllerPosLC:
-            # 左侧居中
-            return (rect.topLeft() + rect.bottomLeft()) / 2 - QPointF(radius, radius)
-
     def initControllers(self):
-        self.controllers:list[CanvasEllipseItem] = []
+        if not hasattr(self, "controllers"):
+            self.controllers:list[CanvasEllipseItem] = []
         rect = self.boundingRect()
         radius = 5
         size = QSizeF(radius*2, radius*2)
         posTypes = [
-            [self.ControllerPosTL, Qt.CursorShape.SizeFDiagCursor], 
-            [self.ControllerPosTC, Qt.CursorShape.SizeVerCursor], 
-            [self.ControllerPosTR, Qt.CursorShape.SizeBDiagCursor], 
-            [self.ControllerPosRC, Qt.CursorShape.SizeHorCursor], 
-            [self.ControllerPosBR, Qt.CursorShape.SizeFDiagCursor], 
-            [self.ControllerPosBC, Qt.CursorShape.SizeVerCursor], 
-            [self.ControllerPosBL, Qt.CursorShape.SizeBDiagCursor], 
-            [self.ControllerPosLC, Qt.CursorShape.SizeHorCursor],
+            [EnumPosType.ControllerPosTL, Qt.CursorShape.SizeFDiagCursor], 
+            [EnumPosType.ControllerPosTC, Qt.CursorShape.SizeVerCursor], 
+            [EnumPosType.ControllerPosTR, Qt.CursorShape.SizeBDiagCursor], 
+            [EnumPosType.ControllerPosRC, Qt.CursorShape.SizeHorCursor], 
+            [EnumPosType.ControllerPosBR, Qt.CursorShape.SizeFDiagCursor], 
+            [EnumPosType.ControllerPosBC, Qt.CursorShape.SizeVerCursor], 
+            [EnumPosType.ControllerPosBL, Qt.CursorShape.SizeBDiagCursor], 
+            [EnumPosType.ControllerPosLC, Qt.CursorShape.SizeHorCursor],
             ]
 
-        for info in posTypes:
-            pos = self.getControllerPosition(rect, radius, info[0])
-            controller = CanvasEllipseItem(info[-1], self)
-            controller.setRect(QRectF(pos, size))
-            self.controllers.append(controller)
+        if len(self.controllers) == 0:
+            for info in posTypes:
+                controller = CanvasEllipseItem(info[-1], self)
+                controller.setRectWrapper(rect, info[0], radius, size)
+                self.controllers.append(controller)
+        else:
+            for controller in self.controllers:
+                controller.resetPosition(rect, radius, size)
 
     def initUI(self):
         self.setFlag(QGraphicsItem.ItemIsSelectable)
