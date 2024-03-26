74f001d49e96f6ef140d0535a94e0fe324172ba9
yaoxuanzhi <yaoxuanzhi@outlook.com>
2024-03-26 17:11:48 +0800

    添加TextItem示例

--- test/canvas_demo.py
+++ test/canvas_demo.py
@@ -1,9 +1,72 @@
 import sys, math
 from PyQt5.QtCore import QRectF
-from PyQt5.QtGui import QPainter
+from PyQt5.QtGui import QPaintEvent, QPainter
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+
+class UICanvasTextItem(QGraphicsTextItem):
+    def __init__(self, parent: QWidget = None) -> None:
+        super().__init__(parent)
+        self.setDefaultFlag()
+        self.setDefaultTextColor(Qt.white)
+
+    # 设置默认模式
+    def setDefaultFlag(self):
+        self.setTextInteractionFlags(Qt.NoTextInteraction)
+        self.setFlags(QGraphicsItem.ItemIsMovable | QGraphicsItem.ItemIsSelectable)
+
+        font = QFont()
+        font.setPointSize(20)
+        self.setFont(font)
+
+    # 取消文本选中状态
+    def cancelSelectedText(self):
+        cursor = self.textCursor()
+        cursor.clearSelection()
+        self.setTextCursor(cursor)
+
+    def isCanEditable(self):
+        '''判断窗口的鼠标是否穿透了'''
+        return (self.textInteractionFlags() | Qt.TextEditorInteraction) == self.textInteractionFlags()
+
+    def switchEditableBox(self):
+        self.clearFocus()
+        self.setTextInteractionFlags(Qt.TextEditorInteraction)
+        self.setFocus()
+
+        textCursor = self.textCursor()
+        pos = math.ceil(len(self.toPlainText())/2)
+        textCursor.setPosition(pos)
+        self.setTextCursor(textCursor)
+
+    def focusInEvent(self, event: QFocusEvent) -> None:
+        if (event.reason() != Qt.PopupFocusReason): # 注意右键菜单在此进入焦点时不保存原始文本
+            self.m_store_str = self.toPlainText() # 保存原始文本
+        return super().focusInEvent(event)
+
+    def focusOutEvent(self, event: QFocusEvent) -> None:
+        if(event.reason() == Qt.MouseFocusReason and QApplication.mouseButtons()== Qt.RightButton):
+            # 右键点击其他地方失去焦点，定义为取消操作，恢复原始文本
+            self.setPlainText(self.m_store_str)
+            self.setTextInteractionFlags(Qt.NoTextInteraction) # 恢复不能编辑状态
+        elif(event.reason() == Qt.PopupFocusReason):
+            #右键弹出菜单时不做处理
+            pass
+        else:
+            #其他情况，包括下面点击回车的情况，编辑成功，发送信号给父对象
+            self.cancelSelectedText()
+            self.setDefaultFlag()
+            # self.mySignal.emit(self.toPlainText())
+        return super().focusOutEvent(event)
+
+    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent) -> None:
+        if(event.button() == Qt.LeftButton):
+            if not self.isCanEditable():
+                # 左键双击进入可编辑状态并打开焦点
+                self.switchEditableBox()
+
+        super().mouseDoubleClickEvent(event)
 
 class CanvasScene(QGraphicsScene):
     def __init__(self, parent=None):
@@ -13,7 +76,14 @@ class CanvasScene(QGraphicsScene):
 
     def initNodes(self):
         rectItem = QGraphicsRectItem(QRectF(-100, -100, 100, 100))
+        rectItem.setFlags(QGraphicsItem.ItemIsSelectable | QGraphicsItem.ItemIsMovable | QGraphicsItem.ItemIsFocusable)
+        rectItem.setAcceptHoverEvents(True)
         self.addItem(rectItem)
+
+        textItem = UICanvasTextItem()
+        textItem.setPlainText("测试文本")
+        textItem.switchEditableBox()
+        self.addItem(textItem)
 
 class CanvasView(QGraphicsView):
     def __init__(self, scene:QGraphicsScene, parent=None):
@@ -68,11 +138,19 @@ class MainWindow(QWidget):
         self.show()
 
     def initUI(self):
+        self.setStyleSheet("QWidget { background-color: #E3212121; }")
+
         self.layout = QVBoxLayout(self)
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.scene = CanvasScene()
         view = CanvasView(self.scene)
         self.layout.addWidget(view)
+
+    def paintEvent(self, a0: QPaintEvent) -> None:
+        backgroundPath = QPainterPath()
+        backgroundPath.setFillRule(Qt.WindingFill)
+
+        return super().paintEvent(a0)
 
 if __name__ == '__main__':
     # enable dpi scale
