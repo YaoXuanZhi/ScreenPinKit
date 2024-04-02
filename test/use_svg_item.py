bb9faeb2fa055997783a830c25d8d4e094180321
yaoxuanzhi <yaoxuanzhi@outlook.com>
2024-04-02 18:41:01 +0800

    添加svg图元

---
+++ test/use_svg_item.py
@@ -0,0 +1,157 @@
+import sys, math
+from PyQt5.QtCore import QRectF
+from PyQt5.QtGui import QMouseEvent, QPaintEvent, QPainter
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QGraphicsSceneDragDropEvent, QGraphicsSceneMouseEvent, QGraphicsSceneWheelEvent, QStyleOptionGraphicsItem, QWidget
+from PyQt5.QtSvg import QSvgWidget
+
+class QDMNodeContentWidget(QSvgWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.initUI()
+
+    def initUI(self):
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0,0,0,0)
+        self.setLayout(self.layout)
+
+        # self.wdg_label = QLabel("Some Title")
+        # self.layout.addWidget(self.wdg_label)
+        # self.layout.addWidget(QTextEdit("foo"))
+
+        if UICanvasSvgItem.markderIndex % 2 == 0:
+            self.load("D:/OpenSource/mini-screenshot-sync/test/resources/zsh.svg")
+        else:
+            self.load("D:/OpenSource/mini-screenshot-sync/test/resources/circles_single.svg")
+
+class UICanvasSvgItem(QGraphicsRectItem):
+    markderIndex = 0
+    def __init__(self, rect: QRectF, parent:QGraphicsItem = None) -> None:
+        super().__init__(rect, parent)
+        self.showText = ""
+        self.setDefaultFlag()
+        UICanvasSvgItem.markderIndex = self.markderIndex + 1
+        self.showText = f"{self.markderIndex}"
+
+        self.content = QDMNodeContentWidget()
+        self.initContent()
+
+    def adjustWidgetSizeToFit(self):
+        '''控件尺寸适配svg图片'''
+        svgBoundRect = QRect(QPoint(0, 0), self.content.renderer().defaultSize())
+        self.setRect(QRectF(svgBoundRect))
+        self.content.setGeometry(svgBoundRect)
+
+    def adjustSvgSizeToFit(self):
+        '''svg图片适配控件尺寸'''
+        self.content.setGeometry(self.boundingRect().toRect())
+
+    def initContent(self):
+        self.grContent = QGraphicsProxyWidget(self)
+        self.grContent.setWidget(self.content)
+        # self.adjustSvgSizeToFit()
+        self.adjustWidgetSizeToFit()
+
+    # 设置默认模式
+    def setDefaultFlag(self):
+        self.setFlags(QGraphicsItem.ItemIsMovable | QGraphicsItem.ItemIsSelectable)
+
+    def paint(self, painter: QPainter, option: QStyleOptionGraphicsItem, widget: QWidget):
+        option.state = option.state & ~QStyle.StateFlag.State_Selected
+        # option.state = option.state & ~QStyle.StateFlag.State_HasFocus
+        return super().paint(painter, option, widget)
+
+class DrawingScene(QGraphicsScene):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # 绘制矩形图元
+        rectItem = QGraphicsRectItem(QRectF(-100, -100, 100, 100))
+        rectItem.setFlags(QGraphicsItem.ItemIsSelectable | QGraphicsItem.ItemIsMovable | QGraphicsItem.ItemIsFocusable)
+        rectItem.setAcceptHoverEvents(True)
+        self.addItem(rectItem)
+
+class DrawingView(QGraphicsView):
+    def __init__(self, scene:QGraphicsScene, parent=None):
+        super().__init__(scene, parent)
+        self.initUI()
+
+    def initUI(self):
+        self.setRenderHints(QPainter.Antialiasing | QPainter.HighQualityAntialiasing | QPainter.TextAntialiasing | QPainter.SmoothPixmapTransform)
+
+        self.setViewportUpdateMode(QGraphicsView.FullViewportUpdate)
+
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        self.scene_width, self.scene_height = 64000, 64000
+        self.scene().setSceneRect(-self.scene_width//2, -self.scene_height//2, self.scene_width, self.scene_height)
+
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        # self.setDragMode(QGraphicsView.RubberBandDrag)
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        if event.button() == Qt.LeftButton:
+            if not self.isCanDrag():
+                item = self.itemAt(event.pos())
+                if item == None:
+                    self.currentItem = UICanvasSvgItem(QRectF(0, 0, 20, 20))
+
+                    self.scene().addItem(self.currentItem)
+
+                    targetPos = self.mapToScene(event.pos())
+                    targetPos.setX(targetPos.x() - self.currentItem.boundingRect().width() / 2)
+                    targetPos.setY(targetPos.y() - self.currentItem.boundingRect().height() / 2)
+                    self.currentItem.setPos(targetPos)
+
+        return super().mousePressEvent(event)
+
+    def isCanDrag(self):
+        '''判断当前是否可以拖曳图元'''
+        matchMode = self.dragMode()
+        return (matchMode | QGraphicsView.RubberBandDrag == matchMode)
+
+    def mouseDoubleClickEvent(self, event: QMouseEvent) -> None:
+        if(event.button() == Qt.RightButton):
+            self.setDragMode(QGraphicsView.RubberBandDrag)
+        elif (event.button() == Qt.LeftButton):
+            self.setDragMode(self.dragMode() & ~QGraphicsView.RubberBandDrag)
+        return super().mouseDoubleClickEvent(event)
+
+class MainWindow(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.initUI()
+        self.show()
+
+    def initUI(self):
+        self.setStyleSheet("QWidget { background-color: #E3212121; }")
+
+        self.layout = QVBoxLayout(self)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.scene = DrawingScene()
+        view = DrawingView(self.scene)
+        self.layout.addWidget(view)
+
+    def paintEvent(self, a0: QPaintEvent) -> None:
+        backgroundPath = QPainterPath()
+        backgroundPath.setFillRule(Qt.WindingFill)
+
+        return super().paintEvent(a0)
+
+if __name__ == '__main__':
+    import sys
+    # enable dpi scale
+    QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QApplication.setAttribute(Qt.ApplicationAttribute.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(Qt.ApplicationAttribute.AA_UseHighDpiPixmaps)
+
+    app = QApplication(sys.argv)
+    # app.setQuitOnLastWindowClosed(False)
+
+    wnd = MainWindow()
+
+    sys.exit(app.exec_())
