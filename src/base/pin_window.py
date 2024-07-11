f740c5a861324cd1034ac5dcc690c938c3b450e9
yaoxuanzhi <yaoxuanzhi@outlook.com>
2024-07-11 18:23:53 +0800

    - 将窗口阴影实现提取成一个QWidget  - 优化焦点传递逻辑  - 同步修改带阴影保存&复制的逻辑

--- src/base/pin_window.py
+++ src/base/pin_window.py
@@ -1,32 +1,30 @@
 # coding=utf-8
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.QtGui import QShowEvent
 from PyQt5.QtWidgets import *
 from .drag_window import *
+from .shadow_window import *
 
 class PinWindow(DragWindow):
     def __init__(self, parent, screenPoint:QPoint, physicalSize:QSize, physicalPixmap:QPixmap, closeCallback:typing.Callable):
         super().__init__(parent)
-        self.shadowWidth = 30
+        self.shadowWidth = 10
         self.roundRadius = 20
-        self.borderLineWidth = 1.5
         self.physicalPixmap = physicalPixmap
-        self.setGeometry(screenPoint.x()-self.shadowWidth, screenPoint.y()-self.shadowWidth, physicalSize.width()+2*self.shadowWidth, physicalSize.height()+2*self.shadowWidth)
-        self.contentRect = self.rect() - QMargins(self.shadowWidth, self.shadowWidth, self.shadowWidth, self.shadowWidth)
+        self.setGeometry(screenPoint.x(), screenPoint.y(), physicalSize.width(), physicalSize.height())
 
         self.defaultFlag()
-        self.initBlink()
 
         self.closeCallback = closeCallback
-        self.unFocusColor = QColor(125, 125, 125, 50)
-        self.focusColor = QColor(255, 0, 255, 50)
-        self.focused = False
-        self.isUseRoundStyle = True
+        self.isUseRoundStyle = False
         self.painter = QPainter()
+
         self.show()
+        self.shadowWindow = ShadowWindow(self.roundRadius, self.shadowWidth, self)
 
     def defaultFlag(self):
-        self.setFocusPolicy(Qt.StrongFocus)
+        self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
         self.setMouseTracking(True)
         self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents, False)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground, True)
@@ -34,95 +32,33 @@ class PinWindow(DragWindow):
 
     def setRoundStyle(self, isUse:bool):
         self.isUseRoundStyle = isUse
-        self.update()
+        self.shadowWindow.setRoundStyle(isUse)
 
     def setShadowColor(self, focusColor:QColor, unFocusColor:QColor):
-        self.focusColor = focusColor
-        self.unFocusColor = unFocusColor
-        self.update()
+        self.shadowWindow.setShadowColor(focusColor, unFocusColor)
 
     def mouseDoubleClickEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
             if self.isAllowDrag():
                 self.close()
 
-    def initBlink(self):
-        self.blinkColors = [Qt.GlobalColor.red, Qt.GlobalColor.yellow, Qt.GlobalColor.green]
-        self.blinkTimers = 0
-        self.blinkColor = self.blinkColors[self.blinkTimers]
-
-        self.blinkTimer = QTimer(self)
-        self.blinkTimer.timeout.connect(self.onBlinkTimerTimeout)
-        self.blinkTimer.start(300)
-
-    def onBlinkTimerTimeout(self):
-        if self.blinkTimers < len(self.blinkColors) - 1:
-            self.blinkTimers += 1
-            self.blinkColor = self.blinkColors[self.blinkTimers]
-            self.update()
-        else:
-            self.blinkTimer.stop()
-            self.blinkColor = None
-            self.update()
-            self.activateWindow()
-
     def isAllowDrag(self):
         return True
-
-    def focusInEvent(self, event:QFocusEvent) -> None:
-        self.focused = True
-        self.update()
-
-    def focusOutEvent(self, event:QFocusEvent) -> None:
-        self.focused = False
-        self.update()
 
     def paintEvent(self, event):
         self.painter.begin(self)
         self.painter.setRenderHint(QPainter.RenderHint.Antialiasing)  # 抗锯齿
 
-    	# 阴影
-        path = QPainterPath()
-        path.setFillRule(Qt.WindingFill)
-        self.painter.fillPath(path, QBrush(Qt.white))
-        if self.focused:
-            color = self.focusColor
-        else:
-            color = self.unFocusColor
-
-        for i in range(10):
-            i_path = QPainterPath()
-            i_path.setFillRule(Qt.WindingFill)
-            ref = QRectF(self.shadowWidth-i, self.shadowWidth-i, self.width()-(self.shadowWidth-i)*2, self.height()-(self.shadowWidth-i)*2)
-            if self.isUseRoundStyle:
-                i_path.addRoundedRect(ref, self.roundRadius, self.roundRadius)
-            else:
-                i_path.addRect(ref)
-            color.setAlpha(int(150 - i**0.5*50))
-            # color.setAlpha(int(150 - math.sqrt(i) * 50))
-            self.painter.setPen(color)
-            self.painter.drawPath(i_path)
-
         if self.isUseRoundStyle:
             clipPath = QPainterPath()
-            clipPath.addRoundedRect(QRectF(self.contentRect), self.roundRadius, self.roundRadius)
+            clipPath.addRoundedRect(QRectF(self.rect()), self.roundRadius, self.roundRadius)
             self.painter.setClipPath(clipPath)
-        self.painter.drawPixmap(self.shadowWidth, self.shadowWidth, self.physicalPixmap)
-
-        # 绘制闪烁边框
-        if self.blinkColor != None:
-            blinkPen = QPen(self.blinkColor)  # 实线，浅蓝色
-            blinkPen.setStyle(Qt.PenStyle.SolidLine)  # 实线SolidLine，虚线DashLine，点线DotLine
-            blinkPen.setWidthF(self.borderLineWidth)  # 0表示线宽为1
-            self.painter.setPen(blinkPen)
-            if self.isUseRoundStyle:
-                self.painter.drawRoundedRect(self.contentRect, self.roundRadius, self.roundRadius)
-            else:
-                self.painter.drawRect(self.contentRect)
+        self.painter.drawPixmap(0, 0, self.physicalPixmap)
 
         self.painter.end()
 
     def closeEvent(self, event) -> None:
+        self.shadowWindow.close()
         if self.closeCallback != None:
             self.closeCallback()
 
@@ -135,3 +71,21 @@ class PinWindow(DragWindow):
             self.move(self.x(), self.y() - 1)
         elif event.key() == Qt.Key_Down:
             self.move(self.x(), self.y() + 1)
+
+    def focusInEvent(self, event:QFocusEvent) -> None:
+        self.shadowWindow.focusInEvent(event)
+
+    def focusOutEvent(self, event:QFocusEvent) -> None:
+        self.shadowWindow.focusOutEvent(event)
+
+    def setWindowOpacity(self, level: float) -> None:
+        self.shadowWindow.setWindowOpacity(level)
+        super().setWindowOpacity(level)  
+
+    def grabWithShaodw(self) -> QPixmap:
+        basePixmap = self.shadowWindow.grab()
+        painter = QPainter()
+        painter.begin(basePixmap)
+        painter.drawPixmap(self.shadowWidth, self.shadowWidth, self.grab())
+        painter.end()
+        return basePixmap
---
+++ src/base/shadow_window.py
@@ -0,0 +1,125 @@
+# coding=utf-8
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtGui import QShowEvent
+from PyQt5.QtWidgets import *
+from .mouse_through_window import *
+
+class ShadowWindow(MouseThroughWindow):
+    def __init__(self, roundRadius:int, shadowWidth:float, parent:QWidget):
+        super(ShadowWindow, self).__init__(parent)
+        self.attachParent = parent
+        self.roundRadius = roundRadius
+        self.shadowWidth = shadowWidth
+        self.borderLineWidth = 0.5
+        self.unFocusColor = QColor(125, 125, 125, 50)
+        self.focusColor = QColor(255, 0, 255, 50)
+        self.focused = False
+        self.isUseRoundStyle = False
+
+        # 根据吸附的窗口大小设置阴影窗口大小
+        self.margins = QMargins(self.shadowWidth, self.shadowWidth, self.shadowWidth, self.shadowWidth)
+        self.setGeometry(self.attachParent.geometry() + self.margins)
+
+        self.defaultFlag()
+        self.painter = QPainter()
+
+        self.attachParent.installEventFilter(self)
+        self.setMouseThroughState(True)
+        self.initBlink()
+        self.show()
+
+    def eventFilter(self, obj, event: QEvent):
+        if event.type() == QEvent.Type.Resize:
+            self.setGeometry(self.attachParent.geometry() + self.margins)
+        elif event.type() == QEvent.Type.Move:
+            self.move(self.attachParent.geometry().topLeft() - QPoint(self.shadowWidth, self.shadowWidth))
+        elif event.type() == QEvent.Type.FocusIn:
+            self.focused = True
+            self.update()
+        elif event.type() == QEvent.Type.FocusOut:
+            self.focused = False
+            self.update()
+        return super().eventFilter(obj, event)
+
+    def defaultFlag(self):
+        self.setMouseTracking(True)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.Window)
+
+    def initBlink(self):
+        self.blinkColors = [Qt.GlobalColor.red, Qt.GlobalColor.yellow, Qt.GlobalColor.green]
+        self.blinkTimers = 0
+        self.blinkColor = self.blinkColors[self.blinkTimers]
+
+        self.blinkTimer = QTimer(self)
+        self.blinkTimer.timeout.connect(self.onBlinkTimerTimeout)
+        self.blinkTimer.start(300)
+
+    def onBlinkTimerTimeout(self):
+        if self.blinkTimers < len(self.blinkColors) - 1:
+            self.blinkTimers += 1
+            self.blinkColor = self.blinkColors[self.blinkTimers]
+            self.update()
+        else:
+            self.blinkTimer.stop()
+            self.blinkColor = None
+            self.update()
+            self.activateWindow()
+
+    def setRoundStyle(self, isUse:bool):
+        self.isUseRoundStyle = isUse
+        self.update()
+
+    def setShadowColor(self, focusColor:QColor, unFocusColor:QColor):
+        self.focusColor = focusColor
+        self.unFocusColor = unFocusColor
+        self.update()
+
+    def focusInEvent(self, event:QFocusEvent) -> None:
+        self.focused = True
+        self.update()
+
+    def focusOutEvent(self, event:QFocusEvent) -> None:
+        self.focused = False
+        self.update()
+
+    def paintEvent(self, event):
+        self.painter.begin(self)
+        self.painter.setRenderHint(QPainter.RenderHint.Antialiasing)  # 抗锯齿
+
+    	# 阴影
+        path = QPainterPath()
+        path.setFillRule(Qt.WindingFill)
+        self.painter.fillPath(path, QBrush(Qt.white))
+        if self.focused:
+            color = self.focusColor
+        else:
+            color = self.unFocusColor
+
+        for i in range(10):
+            i_path = QPainterPath()
+            i_path.setFillRule(Qt.WindingFill)
+            ref = QRectF(self.shadowWidth-i, self.shadowWidth-i, self.width()-(self.shadowWidth-i)*2, self.height()-(self.shadowWidth-i)*2)
+            if self.isUseRoundStyle:
+                i_path.addRoundedRect(ref, self.roundRadius, self.roundRadius)
+            else:
+                i_path.addRect(ref)
+            color.setAlpha(int(150 - i**0.5*50))
+            self.painter.setPen(color)
+            self.painter.drawPath(i_path)
+
+        # 绘制闪烁边框
+        if self.blinkColor != None:
+            blinkPen = QPen(self.blinkColor)  # 实线，浅蓝色
+            blinkPen.setStyle(Qt.PenStyle.SolidLine)  # 实线SolidLine，虚线DashLine，点线DotLine
+            blinkPen.setWidthF(self.borderLineWidth)  # 0表示线宽为1
+            self.painter.setPen(blinkPen)
+            rect = QRect(0, 0, self.width(), self.height())
+            rect = rect - self.margins
+            if self.isUseRoundStyle:
+                self.painter.drawRoundedRect(rect, self.roundRadius, self.roundRadius)
+            else:
+                self.painter.drawRect(rect)
+
+        self.painter.end()
