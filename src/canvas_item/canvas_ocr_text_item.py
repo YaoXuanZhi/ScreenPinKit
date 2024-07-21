5c170c70966fae29f0412ae3ad07ecc126c09886
yaoxuanzhi <yaoxuanzhi@outlook.com>
2024-07-21 22:34:17 +0800

    让CanvasOcrTextItem自动适配字距

--- src/canvas_item/canvas_ocr_text_item.py
+++ src/canvas_item/canvas_ocr_text_item.py
@@ -42,20 +42,76 @@ class QDMNodeContentWidget(QWidget):
         self.layout.setContentsMargins(0,0,0,0)
         self.setLayout(self.layout)
 
-        font = QFont("Microsoft YaHei", 10)
+        font = QFont("Microsoft YaHei", 2)
         # font = QFont("Arial", 1)
-        CanvasUtil.adjustFontSizeToFit(text, font, rect, 1, 100)
 
-        lineEdit = QAutoSizeLineEdit()
-        lineEdit.setFont(font)
-        lineEdit.setMinimumSize(rect.size().toSize())
-        lineEdit.setFixedSize(rect.size().toSize())
-        lineEdit.initStyle()
-        lineEdit.setTextMargins(0, 0, 0, 0)
-        lineEdit.setContentsMargins(0, 0, 0, 0)
-        lineEdit.setText(text)
-        lineEdit.adjustSize()
-        self.layout.addWidget(lineEdit)
+        self.lineEdit = QAutoSizeLineEdit()
+        self.lineEdit.setFont(font)
+        self.lineEdit.setMinimumSize(rect.size().toSize())
+        self.lineEdit.setFixedSize(rect.size().toSize())
+        self.lineEdit.initStyle()
+        self.lineEdit.setTextMargins(0, 0, 0, 0)
+        self.lineEdit.setContentsMargins(0, 0, 0, 0)
+        self.lineEdit.setText(text)
+        self.layout.addWidget(self.lineEdit)
+
+        # 自适应字体大小和字符间距
+        self.adjustFontSizeAndSpacing()
+
+    def adjustFontSizeAndSpacing(self):
+        font = self.lineEdit.font()
+        rect = self.lineEdit.rect()
+        fontSize = 1
+
+        low, high = 1, 100
+        while low <= high:
+            mid = (low + high) // 2
+            font.setPointSize(mid)
+            self.lineEdit.setFont(font)
+
+            # 获取文本的渲染大小
+            textSize = self.getTextSize(font, -1)
+
+            if textSize.width() <= rect.width() and textSize.height() <= rect.height():
+                fontSize = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        # 设置最终的字体大小和字符间距
+        font.setPointSize(fontSize)
+
+        if textSize.width() < rect.width():
+            finalLetterSpacing = self.findBestLetterSpacing(font, rect)
+            font.setLetterSpacing(QFont.AbsoluteSpacing, finalLetterSpacing)
+        
+        self.lineEdit.setFont(font)
+
+    def findBestLetterSpacing(self, font, rect):
+        letterSpacing = 1
+
+        while True:
+            # 获取文本的渲染大小
+            textSize = self.getTextSize(font, letterSpacing)
+
+            if textSize.width() <= rect.width():
+                letterSpacing = letterSpacing + 1
+            else:
+                break
+
+        return letterSpacing
+
+    def getTextSize(self, font, letterSpacing):
+        # 设置字体和字符间距
+        if letterSpacing >= 0:
+            font.setLetterSpacing(QFont.AbsoluteSpacing, letterSpacing)
+            self.lineEdit.setFont(font)
+
+        # 获取文本的渲染大小
+        metrics = QFontMetrics(font)
+        finalSize = metrics.size(0, self.lineEdit.text())
+
+        return finalSize
 
 class CanvasOcrTextItem(QGraphicsRectItem):
     def __init__(self, rect: QRectF, text:str, parent:QGraphicsItem = None) -> None:
