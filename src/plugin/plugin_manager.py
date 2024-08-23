25b04e97d259d3048b54e736ef77f71064cd06cb
yaoxuanzhi <yaoxuanzhi@outlook.com>
2024-08-23 23:21:14 +0800

    将插件机制拆分出外部和内部目录

--- src/plugin/plugin_manager.py
+++ src/plugin/plugin_manager.py
@@ -1,32 +1,45 @@
-import os, sys
+import os, sys, glob
 import importlib
 from .plugin_interface import PluginInterface, GlobalEventEnum
 from common import *
 
 class PluginManager:
-    def __init__(self, plugin_dir):
-        # self.plugin_dir = plugin_dir
-        self.plugin_dir = "../plugins_demos"
+    def __init__(self):
         self.plugins = []
 
     def loadPlugins(self):
-        sys.path.append(self.plugin_dir)
-        for filename in os.listdir(self.plugin_dir):
-            if filename.endswith('.py'):
-                module_name = filename[:-3]
-                module_path = f"{module_name}"
-                module = importlib.import_module(module_path)
+        self.__loadPluginsInside()
+        self.__loadPluginsOutside()
+
+    def __loadPluginsInside(self):
+        folderPath = os.path.dirname(__file__)
+        folderPath = os.path.join(folderPath, "internal_plugins")
+        sys.path.append(folderPath)
+        self.__loadPluginsByFolder(folderPath)
+
+    def __loadPluginsOutside(self):
+        folderPath = cfg.get(cfg.pluginsFolder)
+        self.__loadPluginsByFolder(folderPath)
+
+    def __loadPluginsByFolder(self, folderPath):
+        sys.path.append(folderPath)
+        pyFiles = glob.glob(f"{folderPath}/*.py", recursive=False)
+        for filePath in pyFiles:
+            filename = os.path.basename(filePath)
+            module_name = filename[:-3]
+            module_path = f"{module_name}"
+            module = importlib.import_module(module_path)
                 
-                for attr_name in dir(module):
-                    attr = getattr(module, attr_name)
-                    if isinstance(attr, type) and issubclass(attr, PluginInterface) and attr != PluginInterface:
-                        pluginInst = attr()
-                        self.plugins.append(pluginInst)
-                        pluginInst.onLoaded()
+            for attr_name in dir(module):
+                attr = getattr(module, attr_name)
+                if isinstance(attr, type) and issubclass(attr, PluginInterface) and attr != PluginInterface:
+                    pluginInst = attr()
+                    self.plugins.append(pluginInst)
+                    pluginInst.onLoaded()
 
     def handleEvent(self, eventName: GlobalEventEnum, *args, **kwargs):
         for plugin0 in self.plugins:
             plugin:PluginInterface = plugin0
             plugin.handleEvent(eventName, *args, **kwargs)
 
-pluginMgr = PluginManager(cfg.get(cfg.pluginsFolder))
+pluginMgr = PluginManager()
